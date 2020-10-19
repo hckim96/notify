3161fc468a616e3fbe10f0acb2e4f2966ae65167
hckim96 <hyeoncheolkim96@gmail.com>
2020-10-20 07:51:49 +0900

    Init

---
+++ notify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notify
+//
+//  Created by 김현철 on 2020/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notify/notifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  notifyApp.swift
+//  notify
+//
+//  Created by 김현철 on 2020/10/20.
+//
+
+import SwiftUI
+
+@main
+struct notifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
